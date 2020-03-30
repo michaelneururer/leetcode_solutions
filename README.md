1f06d01ea2067a5656ad23a01c7d374b7dfe944e
Michael Neururer <neururer@mathematik.tu-darmstadt.de>
2020-03-30 23:56:58 +0200

    added automatic readme generator

---
+++ README.md
@@ -0,0 +1,8 @@
+
+## My solutions to leet code problems.
+
+| # | Title | Difficulty |
+|---| ----- | ---------- |
+
+|1|two-sum|eas|
+|8|string-to-integer-atoi|medium|
---
+++ produce_list_of_solved_problems.py
@@ -0,0 +1,19 @@
+from os import listdir
+f = open('README.md','w')
+f.write(r'''
+## My solutions to leet code problems.
+
+| # | Title | Difficulty |
+|---| ----- | ---------- |
+''')
+problems = []
+for file in listdir():
+    if file == 'produce_list_of_solved_problems.py' or file[-3:] != '.py':
+        continue
+    num, name, difficulty = file.rstrip('.py').split('_')
+    problems.append([num,name,difficulty])
+problems.sort(key=lambda x: x[0])
+for prob in problems:
+    f.write('\n')
+    f.write('|{0}|{1}|{2}|'.format(*prob))
+f.close()
