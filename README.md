da3134fe77153538b918a14f0b4f31f781c6f5a6
Michael Neururer <neururer@mathematik.tu-darmstadt.de>
2020-03-31 11:23:23 +0200

    solved more problems

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
 
 ## My solutions to leet code problems.
 
+Solved 2 easy problems, 5 medium problems, and 2 hard problems.
+
 | # | Title | Difficulty |
 |---| ----- | ---------- |
 |1|two-sum|easy|
+|123|best-time-to-buy-and-sell-stock-iii|hard|
 |2|add-two-numbers|medium|
 |3|longest-substring-without-repeating-characters|medium|
 |4|median-of-two-sorted-arrays|hard|
 |5|longest-palindromic-substring|medium|
 |6|zigzag-conversion|medium|
+|7|reverse-integer|easy|
 |8|string-to-integer-atoi|medium|
--- produce_list_of_solved_problems.py
+++ produce_list_of_solved_problems.py
@@ -1,11 +1,5 @@
 from os import listdir
 f = open('README.md','w')
-f.write(r'''
-## My solutions to leet code problems.
-
-| # | Title | Difficulty |
-|---| ----- | ---------- |
-''')
 problems = []
 for file in listdir():
     if file == 'produce_list_of_solved_problems.py' or file[-3:] != '.py':
@@ -13,6 +7,14 @@ for file in listdir():
     num, name, difficulty = file.rstrip('py').rstrip('.').split('_')
     problems.append([num,name,difficulty])
 problems.sort(key=lambda x: x[0])
+f.write(r'''
+## My solutions to leet code problems.
+
+Solved {0} easy problems, {1} medium problems, and {2} hard problems.
+
+| # | Title | Difficulty |
+|---| ----- | ---------- |
+'''.format(len([prob for prob in problems if prob[2]=='easy']), len([prob for prob in problems if prob[2]=='medium']), len([prob for prob in problems if prob[2]=='hard'])))
 for prob in problems:
     f.write('|{0}|{1}|{2}|'.format(*prob))
     f.write('\n')
